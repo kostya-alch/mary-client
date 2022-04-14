4fea9488962664300f9c17cc67904d4e4e5ffbe2
kostya-alch <kostafrompiter@gmail.com>
2022-04-14 16:42:58 +0300

    tailwind config components

--- tailwind.config.js
+++ tailwind.config.js
@@ -75,5 +75,35 @@ module.exports = {
          },
       },
    },
-   plugins: [],
+   plugins: [
+      plugin(({ addComponents, theme, addUtilities }) => {
+         addComponents({
+            '.btn-primary': {
+               backgroundColor: primary,
+               color: '#fff',
+               borderRadius: '0.65rem',
+               transition: 'background-color .3s ease-in-out',
+               '&:hover': {
+                  backgroundColor: '#ff0009',
+               },
+            },
+            '.text-link': {
+               textUnderlineOffset: 4,
+               color: 'rgba(255, 255, 255, .9)',
+               transition: 'text-decoration-color .3s ease-in-out',
+               textDecorationLine: 'underline',
+               textDecorationColor: 'rgba(255, 255, 255, 0.2)',
+               '&:hover': {
+                  textDecorationColor: 'rgba(255, 255, 255, 0.9)',
+               },
+            },
+            '.air-block': {
+               borderRadius: theme('borderRadius.layout'),
+               backgroundColor: theme('colors.gray.950'),
+               color: theme('colors.white'),
+               boxShadow: theme('boxShadow.lg'),
+            },
+         });
+      }),
+   ],
 };
