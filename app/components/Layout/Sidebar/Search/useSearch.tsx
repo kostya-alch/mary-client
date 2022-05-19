e6524761edcbf57441d64997ef4f679ada3bc707
kostya-alch <kostafrompiter@gmail.com>
2022-05-19 16:33:57 +0300

    fix imports

--- app/components/Layout/Sidebar/Search/useSearch.tsx
+++ app/components/Layout/Sidebar/Search/useSearch.tsx
@@ -1,6 +1,9 @@
 import { useDebounce } from '@/hooks/useDebounce';
+
 import { MovieService } from '@/services/movie.service';
+
 import { ChangeEvent, useState } from 'react';
+
 import { useQuery } from 'react-query';
 
 export const useSearch = () => {
