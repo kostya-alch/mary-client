dcb747b85eaffdca92cc90adb56249d04e1cc8f1
kostya-alch <kostafrompiter@gmail.com>
2022-05-19 16:32:02 +0300

    custom hook useSearch

--- app/components/Layout/Sidebar/Search/useSearch.tsx
+++ app/components/Layout/Sidebar/Search/useSearch.tsx
@@ -1,7 +1,24 @@
 import { useDebounce } from '@/hooks/useDebounce';
-import { useState } from 'react';
+import { MovieService } from '@/services/movie.service';
+import { ChangeEvent, useState } from 'react';
+import { useQuery } from 'react-query';
 
 export const useSearch = () => {
 	const [searchTerm, setsearchTerm] = useState('');
 	const debouncedSearch = useDebounce(searchTerm, 500);
+
+	const { isSuccess, data } = useQuery(
+		['search movie list', debouncedSearch],
+		() => MovieService.getAllMovies(debouncedSearch),
+		{
+			select: ({ data }) => data,
+			enabled: !!debouncedSearch,
+		}
+	);
+
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		setsearchTerm(e.target.value);
+	};
+
+	return { isSuccess, handleSearch, data };
 };
---
+++ app/services/movie.service.ts
@@ -0,0 +1,13 @@
+import { IMovie } from './../shared/types/movie.types';
+
+import { getMoviesUrl } from '@/config/api.config';
+
+import { instanceAxios } from './../api/interceptors';
+
+export const MovieService = {
+	async getAllMovies(searchTerm?: string) {
+		return instanceAxios.get<IMovie[]>(getMoviesUrl(``), {
+			params: searchTerm ? { searchTerm } : {},
+		});
+	},
+};
